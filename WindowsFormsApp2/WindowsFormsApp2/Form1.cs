0a16f78ae1f2b49d43a9596bd2c31b88a5b24e8d
de3key <gnosthard@gmail.com>
2021-09-24 13:44:15 +0700

    project

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        int pr = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pr = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pr = pr + 1;
+            progressBar1.Value = pr;
+            toolStripStatusLabel1.Text = pr + "%";
+        }
+    }
+}
